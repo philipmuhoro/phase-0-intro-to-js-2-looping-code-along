b8ea75c9cecc2e485faf273d255e7e2dc73b2f6f
philipwanjiku <muhorophilip7@gmail.com>
2023-10-14 10:37:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names=["Philip", "Gakosh", "Muhoro"];
+const event=["suprise"]
+function writeCards(names,event){
+    const message =[]
+    for(const name of names){
+        message.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return message
+
+}
+
+function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+    }
+}
